8281aa8c131ebd87c3f51b5310e52f0b85273375
Carlton369 <carltonyxz@gmail.com>
2024-07-22 00:54:52 +0800

    loaded firestore, able to extract data and display on screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-
 import Link from 'next/link'
 import {Navbar} from '../app/navbar'
 import '../css/page.css'
+import CataloguePage from './catalogue'
+
 
 export default function Page() {
   return (
     <div className = 'wrapper'>
       <Navbar />
       <Link href="/dashboard">Dashboard</Link>
+      <CataloguePage />
     </div>
   )
 }
